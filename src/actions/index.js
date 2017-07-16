7da95031595ed9e7a1bcf2108232cd7a2afb1847
June Shaw <june.shaw@me.com>
2017-07-15 18:44:19 -0600

    Adding reducer and action for fetchWeather

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,20 @@
-const API_KEY = 'e9b551b44d587effa6f263b9f8a93121
-';
+import axios from 'axios';
+
+const API_KEY = 'e9b551b44d587effa6f263b9f8a93121';
 
 const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
-export function fetchWeather() {
+export function fetchWeather(city) {
+  const url = `http://${ROOT_URL}&q=${city},us`;
+  console.log(url);
+  const request = axios.get(url);
+
+  console.log('request: ', request);
+  
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   };
 }
